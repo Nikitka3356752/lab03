74ef07f1fcb3b1915f079a2af9684df0154eba5a
eJwtzMtqwzAQQNF/mbUWUVoplnZtoZSQpNCUQldiLE1SYdkSIzsPQv69ELy9cO4N0Huq9Tt3NIAFZbxsG0ULucCl14HQ60Y9BUMHXCnVKqOpCWoJAgrnUwzEM73NJzfOq+Nfdvv3j81O/+LPm7ysX7mXn4a/pq0+rV82e35upFlVEPAgbrwWAgstIRODgOrzI2Do42DL1KboXUdXwYTBZj4KppLFVIkt9RiTOGfuDimfQQBdSmSqDkeww5TS/f4PKJBPhg== <nikita.tlewasov@yandex.ru>
2024-09-24 23:12:56 +0500

    3

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def bubble(arr):
+def bubble(arr, ascending=True):
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
+            if (ascending and arr[j] > arr[j+1]) or (not ascending and arr[j] < arr[j+1]):
                 arr[j], arr[j+1] = arr[j+1], arr[j]  # Меняем элементы местами
     return arr
 
@@ -13,7 +13,21 @@ for i in range(n):
     num = float(input(f"Введите число {i+1}: "))
     numbers.append(num)
 
-sorted_numbers = bubble(numbers)
+order = input("Введите направление сортировки (возрастание/убывание): ").strip().lower()
 
-print("Числа после сортировки по возрастанию:")
+if order == "возрастание":
+    ascending = True
+elif order == "убывание":
+    ascending = False
+else:
+    print("Некорректный ввод. Сортировка по возрастанию по умолчанию.")
+    ascending = True
+
+sorted_numbers = bubble(numbers, ascending)
+
+if ascending:
+    print("Числа после сортировки по возрастанию:")
+else:
+    print("Числа после сортировки по убыванию:")
+
 print(sorted_numbers)
